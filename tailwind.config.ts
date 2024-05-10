525f4e5b5e262ceffe86a7ae10f36d120a4a3068
MuhammadFahrulazmi322 <fazmi322@gmail.com>
2024-05-10 15:41:01 +0700

    initial project and design system

---
+++ tailwind.config.ts
@@ -0,0 +1,66 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  mode:"jit",
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors:{
+        primary:{
+          blue:"#1053B7",
+          white:"#FFFFFF",
+          black:"#050D18",
+        },
+        secondary:{
+          yellow:"#EBA41F",
+          red:"#EB1F2B"
+        },
+        textColor:{
+          primary:"#050D18",
+          secondary:"#50555D"
+        },
+        white:"#FFFFFF",
+        black:"#000000",
+        gray:{
+          10:"#E6E6E7",
+          30:"#B4B6B9",
+          50:"#82868B",
+          70:"#50555D",
+          100:"#050D18"
+        },
+        blue:{
+          10:"#E7EDF7",
+          30:"#B7CBE9",
+          50:"#87A9DB",
+          70:"#5786CC",
+          100:"#1053B7"
+        },
+        red:{
+          10:"#FDE8E9",
+          30:"#F9BBBF",
+          50:"#F58F95",
+          70:"#F1626A",
+          100:"#EB1F2B"
+        },
+        yellow:{
+          10:"#FDF5E8",
+          30:"#F9E3BB",
+          50:"#F5D18F",
+          70:"#F1BF62",
+          100:"#EBA41F"
+        }
+      }
+    },
+  },
+  plugins: [],
+};
+export default config;
